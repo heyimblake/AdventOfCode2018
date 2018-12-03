1b8c13f8dfa54735f581b89f4f42cb49a7c11dc9
Blake Khan <blake@blakekhan.com>
2018-12-03 13:42:04 -0500

    Add Day 1 Part 2, refactor, add tests

---
+++ day1/ChronalCalibration.go
@@ -0,0 +1,90 @@
+package day1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+/*
+	Part 1
+ */
+func GetResultingFrequency(startingFrequency int, filePath string) int {
+	fileScannerDoublePointer, fileDoublePointer := getScanner(filePath)
+	scanner := *fileScannerDoublePointer
+	file := *fileDoublePointer
+	result := startingFrequency
+
+	// Ensure file will be closed when the function is finished
+	defer file.Close()
+
+	// Scan each line
+	for scanner.Scan() {
+		line := scanner.Text()
+		freq, _ := strconv.Atoi(line)
+		result += freq
+	}
+
+	return result
+}
+
+/*
+	Part 2
+ */
+func GetFirstRepeatingFrequency(startingFrequency int, filePath string, timesRepeated int) int {
+	var dupes = make(map[int]int)// Key: Frequency, Value: # times appeared
+
+	// Add starting frequency to map
+	dupes[startingFrequency] = 1
+
+	// Current Frequency
+	result := startingFrequency
+
+	for true {
+		fileScanner, file := getScanner(filePath)
+
+		// Scan each line
+		for (*fileScanner).Scan() {
+			line := (*fileScanner).Text()
+			freq, _ := strconv.Atoi(line)
+			result += freq
+
+			// Update Map
+			dupes[result]++
+
+			// Check for first duplicate
+			if dupes[result] == timesRepeated {
+				(*file).Close()
+				return result
+			}
+		}
+
+		// Close file
+		(*file).Close()
+	}
+
+	panic("Repeating frequency not found!")
+	return -1
+}
+
+func check(e error) {
+	if e != nil {
+		fmt.Printf("ERROR: Cannot open file. Does it exist?\n")
+		os.Exit(1)
+	}
+}
+
+func getScanner(filePath string) (**bufio.Scanner, **os.File) {
+	// Open Data File
+	file, err := os.Open(filePath)
+
+	// Check if we can continue
+	check(err)
+
+	// Create scanner
+	scanner := bufio.NewScanner(file)
+
+	// Return scanner and file double pointers
+	return &scanner, &file
+}
---
+++ day1/ChronalCalibration_test.go
@@ -0,0 +1,21 @@
+package day1
+
+import "testing"
+
+func TestGetResultingFrequency(t *testing.T) {
+	expected := 553
+	actual := GetResultingFrequency(0, "Day1.txt")
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d\n", expected, actual)
+	}
+}
+
+func TestGetFirstRepeatingFrequency(t *testing.T) {
+	expected := 78724
+	actual := GetFirstRepeatingFrequency(0, "Day1.txt", 2)
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d\n", expected, actual)
+	}
+}
