ad02838194ab92f9b4f3377b2bf2222979015b0d
Blake Khan <blake@blakekhan.com>
2018-12-05 01:59:53 -0500

    Add Day 3

---
+++ day3/fabric.go
@@ -0,0 +1,134 @@
+package day3
+
+import (
+	"fmt"
+	"github.com/heyimblake/AdventOfCode-2018/util"
+	"strconv"
+	"strings"
+)
+
+type Point struct {
+	X int
+	Y int
+	Claims []*Claim
+}
+
+type Claim struct {
+	Id int
+	FromLeft int
+	FromTop int
+	Width int
+	Height int
+	Points []*Point
+}
+
+func CreateClaim(data string) *Claim {
+	splitData := strings.Split(data, " ")
+	fromNums := strings.Split(splitData[2], ",") // Ex: "45" "64:"
+	dimensions := strings.Split(splitData[3], "x")
+
+	id, _ := strconv.Atoi(splitData[0][1:]) // Remove the # symbol
+	fromLeft, _ :=  strconv.Atoi(fromNums[0])
+	fromTop, _ := strconv.Atoi(fromNums[1][:len(fromNums[1]) - 1]) // Remove the : symbol
+	width, _ := strconv.Atoi(dimensions[0])
+	height, _ := strconv.Atoi(dimensions[1])
+
+	claim := Claim{id, fromLeft, fromTop, width, height, nil}
+
+	return &claim
+}
+
+func ClaimToString(claim *Claim) string {
+	return fmt.Sprintf("#%d @ %d,%d: %dx%d", claim.Id, claim.FromLeft, claim.FromTop, claim.Width, claim.Height)
+}
+
+func LoadClaimsFromFile(filePath string) []*Claim {
+	claims := make([]*Claim, 0)
+
+	content, _ := util.GetFileContents(filePath)
+
+	for _, line := range *content {
+		claims = append(claims, CreateClaim(line))
+	}
+
+	return claims
+}
+
+func PrepareClaims(filePath string) []*Claim {
+	// Read file and make a Claim struct for each one
+	claims := LoadClaimsFromFile(filePath)
+
+	// Map string to point pointer, so we can ensure we are using the same reference
+	stringPointMap := make(map[string]*Point)
+
+
+	// Calculate the points of each claim and append it to the array in the struct
+	for _, claim := range claims {
+		claim.Points = make([]*Point, 0)
+
+		for x := claim.FromLeft; x < claim.FromLeft + claim.Width; x++ {
+			for y := claim.FromTop; y < claim.FromTop + claim.Height; y++ {
+				point := stringPointMap[fmt.Sprintf("{%d %d}", x, y)]
+
+				if point == nil {
+					point = &Point{x, y, make([]*Claim, 0)}
+					stringPointMap[fmt.Sprintf("{%d %d}", x, y)] = point
+				}
+
+				claim.Points = append(claim.Points, point)
+				point.Claims = append(point.Claims, claim)
+			}
+		}
+	}
+
+	return claims
+}
+
+/*
+	Part 1
+ */
+func FindOverlappingSquareInches(claims []*Claim) int {
+	// Create Map to keep track of how many occurrences of each point there are
+	pointOccurrences := make(map[*Point]int)
+
+	// Go through all points of each Claim and add to map as needed
+	for _, claim := range claims {
+		for _, point := range claim.Points{
+			pointOccurrences[point]++
+		}
+	}
+
+	sqInchCount := 0
+
+	// Find which occurred more than once
+	for _, count := range pointOccurrences {
+		if count > 1 {
+			sqInchCount++
+		}
+	}
+
+	return sqInchCount
+}
+
+func FindNonOverlappingClaim(claims []*Claim) *Claim {
+	// Find the Claim where all its points has 1 claim to it
+	for _, claim := range claims {
+		passed := true
+
+		pointsLoop:
+		for _, point := range claim.Points {
+			if len(point.Claims) != 1 {
+				passed = false
+				break pointsLoop
+			}
+		}
+
+		// We found it!!! Woo!!
+		if passed {
+			return claim
+		}
+	}
+
+	// Or if we couldn't find anything :(
+	return nil
+}
---
+++ day3/fabric_test.go
@@ -0,0 +1,93 @@
+package day3
+
+import (
+	"github.com/heyimblake/AdventOfCode-2018/util"
+	"testing"
+)
+
+func TestCreateClaim(t *testing.T) {
+	contents, _ := util.GetFileContents("Day3.txt")
+
+	claim := CreateClaim((*contents)[3])
+
+	if !claimEquals(&claim, 4, 694, 889, 28, 28) {
+		t.Fatalf("Claim does not match. (Case 1)")
+	}
+
+	claim = CreateClaim((*contents)[49])
+
+	if !claimEquals(&claim, 50, 310, 86, 11, 27) {
+		t.Fatalf("Claim does not match. (Case 2)")
+	}
+
+	claim = CreateClaim((*contents)[1300])
+
+	if !claimEquals(&claim, 1301, 130, 498, 26, 10) {
+		t.Fatalf("Claim does not match. (Case 3)")
+	}
+}
+
+func TestClaimToString(t *testing.T) {
+	cases := []string{
+		"#1234 @ 23,232: 91x53",
+		"#129842 @ 12414124132,345345346: 28294x9",
+		"#1284 @ 347,704: 27x23",
+		"#830 @ 815,683: 16x22",
+		"#-1 @ 0,214: 2x23",
+	}
+
+	for _, expected := range cases {
+		actual := ClaimToString(CreateClaim(expected))
+
+		if actual != expected {
+			t.Fatalf("Expected \"%s\" but got \"%s\"\n", expected, actual)
+		}
+	}
+}
+
+func TestLoadClaimsFromFile(t *testing.T)  {
+	claims := LoadClaimsFromFile("Day3.txt")
+	actual := claims[493]
+
+	if !claimEquals(&actual, 494, 651, 815, 25, 18) {
+		t.Fatalf("Claim does not match. (Case 1)")
+	}
+
+	actual = claims[7]
+
+	if !claimEquals(&actual, 8, 612, 291, 21, 17) {
+		t.Fatalf("Claim does not match. (Case 2)")
+	}
+
+	actual = claims[241]
+
+	if !claimEquals(&actual, 242, 178, 287, 22, 23) {
+		t.Fatalf("Claim does not match. (Case 3)")
+	}
+}
+
+func TestFindOverlappingSquareInches(t *testing.T)  {
+	expected := 104712
+	actual := FindOverlappingSquareInches(PrepareClaims("Day3.txt"))
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d\n", expected, actual)
+	}
+}
+
+func TestFindNonOverlappingClaim(t *testing.T)  {
+	expected := 840
+	actual := FindNonOverlappingClaim(PrepareClaims("Day3.txt")).Id
+
+	if actual != expected {
+		t.Fatalf("Expected %d but got %d\n", expected, actual)
+	}
+}
+
+func claimEquals(claim **Claim, id int, fromLeft int, fromTop int, width int, height int) bool {
+	if (*claim).Id != id || (*claim).FromLeft != fromLeft || (*claim).FromTop != fromTop || (*claim).Width != width || (*claim).Height != height {
+		return false
+	}
+
+	return true
+}
